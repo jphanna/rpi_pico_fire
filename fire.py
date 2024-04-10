77182f749b0c74f81a9de043f42cc5d865ce4966
John Hanna <jph_jr@yahoo.com>
2024-04-10 13:05:39 -0400

    change utime and urandom to time and random

--- fire.py
+++ fire.py
@@ -1,6 +1,6 @@
 import machine
-import utime
-import urandom
+import time
+import random
 
 
 class Fire(object):
@@ -10,25 +10,25 @@ class Fire(object):
         self.fire_type = fire_type
         self.flicker_speed = self._get_flicker_speed()
         self.brightness = self._get_brightness()
-        self.current_millis = utime.ticks_ms()
+        self.current_millis = time.ticks_ms()
 
     def _get_flicker_speed(self):
-        return urandom.randrange(200)  # 100ms
+        return random.randrange(200)  # 100ms
 
     def _get_brightness(self):
         if self.fire_type == "lantern":
-            return urandom.randrange(25000, 65535)
+            return random.randrange(25000, 65535)
         elif self.fire_type == "candle":
-            return urandom.randrange(2000, 8000)
+            return random.randrange(2000, 8000)
         else:
-            return urandom.randrange(34695, 65535)
+            return random.randrange(34695, 65535)
 
     def flicker(self):
         if (
-            utime.ticks_diff(utime.ticks_ms(), self.current_millis)
+            time.ticks_diff(time.ticks_ms(), self.current_millis)
             >= self.flicker_speed
         ):
             self.led.duty_u16(self.brightness)
             self.flicker_speed = self._get_flicker_speed()
             self.brightness = self._get_brightness()
-            self.current_millis = utime.ticks_ms()
+            self.current_millis = time.ticks_ms()
